f996af5505b08b2e351fc0d570a478c96b1d867e
Michal Czyzowicz <czyzowicz.m@gmail.com>
2021-01-07 17:56:55 +0100

    add innerHTML

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const body = document.body;
-body.append("Whatever");
+// body.append("Whatever");
 
-// const div = document.createElement('div');
+const div = document.createElement('div');
 // div.innerText ="Hello World";
-// div.textContent = "Hello World 2";
-// body.append(div);
+//div.textContent = "Hello World 2";
+div.innerHTML = "<strong>Hello World 2</strong>";
+const strong = document.createElement('strong');
+strong.innerText ="Hello World 2";
+div.append(strong);
 
-const div = document.querySelector('div');
-console.log(div.textContent);
-console.log(div.innerText);
+body.append(div);
+
+// const div = document.querySelector('div');
+// console.log(div.textContent);
+// console.log(div.innerText);
