001d75b18f5e6a677c43a5dc1c0b64d08568ee72
Michal Czyzowicz <czyzowicz.m@gmail.com>
2021-01-07 15:07:25 +0100

    query selectors

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-// const body = document.body;
-// body.append("Hello");
+const body = document.body;
+body.append("Whatever");
 
 // const div = document.createElement('div');
-// //div.innerText ="Hello World";
+// div.innerText ="Hello World";
 // div.textContent = "Hello World 2";
-
 // body.append(div);
 
 const div = document.querySelector('div');
